a7c7365c7fb7b4fe4af5237f0f16e085bfd6033d
kiki-eng <abosedekikelomo408@gmail.com>
2022-12-15 15:19:04 -0500

    FinalProject

---
+++ script.js
@@ -0,0 +1,34 @@
+var dropdownBtn = document.getElementById("dropdown-btn");
+var logo = document.getElementById("logo");
+var menu = document.getElementById("menu");
+var icon = document.getElementById("icon");
+
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+dropdownBtn.addEventListener("click", function (e) {
+  if (menu.className === "links-cont" && logo.className === "logo-cont") {
+    menu.classList.add("responsive");
+    logo.classList.add("responsive");
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+  } else {
+    menu.className = "links-cont";
+    logo.className = "logo-cont";
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    menu.classList.remove("responsive");
+    logo.classList.remove("responsive");
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
